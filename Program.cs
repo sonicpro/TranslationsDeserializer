3c6e812cc3b466ac21b7588e3ee0307e4bc9f681
Volodymyr Tsurko <volodymyr.tsurko@qognify.com>
2022-08-23 11:56:09 +0300

    Added json that contains the translations.

--- Program.cs
+++ Program.cs
@@ -1,17 +1,14 @@
 ﻿using Amazon.Lambda.Serialization.SystemTextJson;
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Text.Json;
-using TranslationsDeserializer.Models;
-
 namespace TranslationsDeserializer
 {
     class Program
     {
         static void Main()
         {
-            byte[] json = File.ReadAllBytes(@"C:\Workitems\SG POC\GENERATE_TITLE_PAGE_captured.json");
+            var path = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            byte[] json = File.ReadAllBytes($"{path}\\GENERATE_TITLE_PAGE_captured.json");
             var serializer = new SerializerWrapper();
             Console.WriteLine(serializer.GetBaseMessage(json));
         }
